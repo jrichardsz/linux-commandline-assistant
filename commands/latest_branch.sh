7e2a12a82d42a24cc60919b0f654bbff3acd82b9
jrichardsz <jrichardsz.java@gmail.com>
2023-10-15 09:35:58 -0500

    improve latest_branch

--- commands/latest_branch.sh
+++ commands/latest_branch.sh
@@ -1,3 +1,7 @@
 git fetch
 latest_branch=$(git for-each-ref --sort=-committerdate | head -n 1 | awk -F '/' '{ print $NF }')
-echo "latest_branch: $latest_branch"
+echo -e "latest_branch: $latest_branch\n"
+
+echo -e "detailed\n"
+
+git log --all --graph --decorate --oneline --simplify-by-decoration
